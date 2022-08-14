e6422d4528c5db08a542c4e806a50c3638a3ea34
Dmitriy Aykalo <aykalo89@gmail.com>
2022-08-14 20:14:39 +0300

    first commit

---
+++ java.js
@@ -0,0 +1,30 @@
+var longWordTests = [
+    {
+        parameters: ["localization"],
+        expectedResult: "l10n"
+    },
+    {
+        parameters: ["internationalization"],
+        expectedResult: "i18n"
+    },
+    {
+        parameters: ["word"],
+        expectedResult: "word"
+    },
+    {
+        parameters: [""],
+        expectedResult: ""
+    },
+    {
+        parameters: ["1"],
+        expectedResult: "1"
+    }
+];
+
+
+function longWord(word) {
+    if   ( parameters.length > 10) {
+        expectedResult = (parameters[0]+[parameters.length-2]+parameters[parameters.length-1]);
+    }
+    else {return expectedResult;}
+}
